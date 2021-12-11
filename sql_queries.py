c69a6db476ed9da23217e22757f0c28f3e0dd1f6
Abdallah El-Sawy <67811312+abdallah-elsawy@users.noreply.github.com>
2021-12-12 01:40:04 +0200

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -87,9 +87,8 @@ INSERT INTO users (
                         gender ,
                         level )
                         VALUES(%s,%s,%s,%s,%s)
-                        ON CONFLICT (user_id) DO NOTHING
+                        ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level
                         """)
-
 song_table_insert = ("""
 INSERT INTO songs (
                         song_id,
